26d9a9df8a9af653743e8f719ce09815f02c6978
smy-101 <shimy1011@foxmail.com>
2024-06-19 17:07:10 +0800

    改写rss_handler

--- utils/rss_handler.rb
+++ utils/rss_handler.rb
@@ -5,13 +5,65 @@ require_relative '../model/rss_feed'
 # format the rss feed
 class RSSHandler
   def initialize(url)
-    @doc = Nokogiri::XML(URI.parse(url).open)
+    @url = url
+  end
+
+  def add_check?
+    return false if exist?
+
+    parse_doc
+    return false unless rss?
+
+    true
+  end
+
+  def add_rss_url
+    print_subscription_prompt
+    subscribe_if_confirmed
+  end
+
+  private
+
+  def exist?
+    RssFeed.find_by_url(@url)
+  end
+
+  def rss?
+    # RSS feeds usually have a <channel> element
+    return true if @doc.at_xpath('//channel')
+
+    # Atom feeds usually have a <feed> element
+    return true if @doc.at_xpath('//feed')
+
+    false
   rescue StandardError
-    # puts "Failed to open URL: #{e.message}"
+    false
+  end
+
+  def parse_doc
+    @doc = Nokogiri::XML(URI.parse(url).open)
+    handle_doc
+  rescue StandardError
     @doc = nil
   end
 
-  def handle
+  def print_subscription_prompt
+    puts "订阅的RSS为#{@title}"
+    puts '是否要订阅这个RSS?(y/n)'
+  end
+
+  def subscribe_if_confirmed
+    return puts '取消订阅' unless gets.chomp == 'y'
+
+    rss_feed = RssFeed.create(url: @url, title: @title)
+    rss_feed.persisted? ? puts('订阅成功') : print_errors(rss_feed)
+  end
+
+  def print_errors(rss_feed)
+    puts "Errors: #{rss_feed.errors.messages}"
+  end
+
+  def handle_doc
     return unless @doc
 
     case @doc.root.name
@@ -24,40 +76,21 @@ class RSSHandler
     end
   end
 
-  def rss_title
-    return nil unless @doc
-
-    title_element = @doc.xpath('//channel/title').first
-    if title_element.nil?
-      nil
-    else
-      title_element.content
-    end
-  end
-
-  def add_rss_url(url)
-    result = RssFeed.create(url: url)
-    puts result.persisted?
-    puts "Failed to create RssFeed: #{result.errors.messages}"
-  end
-
-  private
-
   def handle_rss
+    @items = []
     @doc.xpath('//item').each do |item|
-      title = item.at_xpath('title').content
-      link = item.at_xpath('link').content
-      [title, link, item]
-      puts [title, link, item]
+      @title = item.at_xpath('title').content
+      @link = item.at_xpath('link').content
+      @items << item
     end
   end
 
   def handle_atom
+    @items = []
     @doc.xpath('//entry').each do |entry|
-      title = entry.at_xpath('title').content
-      link = entry.at_xpath('link')['href']
-      [title, link, item]
-      puts [title, link, item]
+      @title = entry.at_xpath('title').content
+      @link = entry.at_xpath('link')['href']
+      @items << entry
     end
   end
 end
