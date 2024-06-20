4190589f1e64707a24128558f821f549bb8482ce
smy-101 <shimy1011@foxmail.com>
2024-06-20 16:50:05 +0800

    继续优化RSSHandler

--- main.rb
+++ main.rb
@@ -6,26 +6,11 @@ loop do
   puts '请输入RSS的订阅链接'
   url = gets.chomp.strip
 
-  # 判断这个url是否与数据库的中的数据重合
-  rss_feed = RssFeed.find_by_url(url)
-  if rss_feed
-    puts '这个RSS已经订阅过了,请输入新的订阅链接'
+  rss_handler = RSSHandler.new(url)
+  if rss_handler.valid_rss_url?
+    # rss_handler.add_rss_url
+    puts '11111111'
   else
-    rss_handler = RSSHandler.new(url)
-    title = rss_handler.rss_title
-    if title
-      puts "订阅的RSS为#{title}"
-      puts '是否要订阅这个RSS?(y/n)'
-      if gets.chomp == 'y'
-        rss_handler.add_rss_url(url)
-        # TODO: 将这个url存入数据库
-        puts '订阅成功'
-      else
-        puts '取消订阅'
-      end
-      break
-    else
-      puts '无法获取到正确的内容,请重新输入URL'
-    end
+    puts 'RSS链接已添加或者不是一个有效的RSS链接'
   end
 end
--- utils/rss_handler.rb
+++ utils/rss_handler.rb
@@ -2,30 +2,20 @@ require 'nokogiri'
 require 'open-uri'
 require_relative '../model/rss_feed'
 
-# format the rss feed
-class RSSHandler
+# parse the rss feed
+class RSSParser
+  attr_reader :title, :rss_url
+
   def initialize(url)
     @url = url
   end
 
-  def add_check?
-    return false if exist?
-
-    parse_doc
-    return false unless rss?
-
-    true
-  end
-
-  def add_rss_url
-    print_subscription_prompt
-    subscribe_if_confirmed
-  end
-
-  private
-
-  def exist?
-    RssFeed.find_by_url(@url)
+  def parse_doc
+    @doc = Nokogiri::XML(URI.parse(@url).open)
+    handle_doc
+  rescue StandardError => e
+    puts "Error: #{e.message}"
+    @doc = nil
   end
 
   def rss?
@@ -36,32 +26,12 @@ class RSSHandler
     return true if @doc.at_xpath('//feed')
 
     false
-  rescue StandardError
+  rescue StandardError => e
+    puts "Error: #{e.message}"
     false
   end
 
-  def parse_doc
-    @doc = Nokogiri::XML(URI.parse(url).open)
-    handle_doc
-  rescue StandardError
-    @doc = nil
-  end
-
-  def print_subscription_prompt
-    puts "订阅的RSS为#{@title}"
-    puts '是否要订阅这个RSS?(y/n)'
-  end
-
-  def subscribe_if_confirmed
-    return puts '取消订阅' unless gets.chomp == 'y'
-
-    rss_feed = RssFeed.create(url: @url, title: @title)
-    rss_feed.persisted? ? puts('订阅成功') : print_errors(rss_feed)
-  end
-
-  def print_errors(rss_feed)
-    puts "Errors: #{rss_feed.errors.messages}"
-  end
+  private
 
   def handle_doc
     return unless @doc
@@ -76,13 +46,19 @@ class RSSHandler
     end
   end
 
+  def extract_data_from_item(item)
+    {
+      title: item.at_xpath('title').content,
+      link: item.at_xpath('link').content,
+      description: item.at_xpath('description').content,
+      pubDate: item.at_xpath('pubDate').content
+    }
+  end
+
   def handle_rss
-    @items = []
-    @doc.xpath('//item').each do |item|
-      @title = item.at_xpath('title').content
-      @link = item.at_xpath('link').content
-      @items << item
-    end
+    @title = @doc.at_xpath('//channel/title').content
+    @rss_url = @doc.at_xpath('//channel/link').content
+    @items = @doc.xpath('//item').map { |item| extract_data_from_item(item) }
   end
 
   def handle_atom
@@ -92,5 +68,52 @@ class RSSHandler
       @link = entry.at_xpath('link')['href']
       @items << entry
     end
+  end
+end
+
+# check the url
+class URLChecker
+  def exist?(url)
+    RssFeed.find_by_url(url)
+  end
+end
+
+# format the rss feed
+class RSSHandler
+  def initialize(url)
+    @url = url
+    @rss_parser = RSSParser.new(url)
+    @url_checker = URLChecker.new
+  end
+
+  def valid_rss_url?
+    @rss_parser.parse_doc
+    return false unless @rss_parser.rss?
+
+    puts @rss_parser.rss_url
+    false if @url_checker.exist?(@rss_parser.rss_url)
+  end
+
+  def add_rss_url
+    print_subscription_prompt
+    subscribe_if_confirmed
+  end
+
+  private
+
+  def print_subscription_prompt
+    puts "订阅的RSS为#{@rss_parser.title}"
+    puts '是否要订阅这个RSS?(y/n)'
+  end
+
+  def subscribe_if_confirmed
+    return puts '取消订阅' unless gets.chomp == 'y'
+
+    rss_feed = RssFeed.create(url: @rss_parser.rss_url, title: @rss_parser.title)
+    rss_feed.persisted? ? puts('订阅成功') : print_errors(rss_feed)
+  end
+
+  def print_errors(rss_feed)
+    puts "Errors: #{rss_feed.errors.messages}"
   end
 end
